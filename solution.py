7cc191ed29d62337ef9816b88ecdc2c6f962d6ea
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2022-06-10 11:49:14 +0200

    Resolve phase1 : Simplicité Python

--- solution.py
+++ solution.py
@@ -1,4 +1,12 @@
 # return string without spaces
-def erase(cc):
-
-    pass
+def erase(str):
+	result = ""
+	for i in range(len(str)):
+		if str[i] == " ":
+			if i != 0 and str[i-1] == " ":
+				result += str[i];
+			elif i+1 < len(str) and str[i+1] == " ":
+				result += str[i]
+		else:
+			result += str[i]
+	return result
