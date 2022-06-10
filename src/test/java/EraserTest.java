2f92391faf4b963c0742f420cb13f993ada2abf4
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2022-06-10 11:20:58 +0200

    Les tests sont de nouveau normaux

--- src/test/java/EraserTest.java
+++ src/test/java/EraserTest.java
@@ -3,35 +3,25 @@ package test.java;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-import main.java.eraser.Eraser;
+import main.java.eraser.Simple;
+import main.java.eraser.Efficace;
 
 public class EraserTest {
     @Test
-    public void testSolution() throws IOException {
-    	long startTime = System.currentTimeMillis();
-    	
-//        assertEquals("666,thenumberofthebeast", Eraser.erase("666, the number of the beast"));
-//        assertEquals("06   07651970", Eraser.erase("06   07 65 19 70 "));
-//        assertEquals("OK", Eraser.erase("OK"));
-//        assertEquals("Coucou  JM  B", Eraser.erase("Cou cou  J M  B"));
-//        assertEquals("", Eraser.erase(""));
-    	
-    	FileReader fr = new FileReader("C:\\Users\\Etudiant\\Downloads\\texteSAE.txt");
-    	
-    	String text = "";
-    	for (int i = fr.read(); i != -1; i = fr.read()) {
-			text += (char) i;
-		}
-    	
-    	System.out.println(text);
-        
-        long endTime = System.currentTimeMillis();
-        System.out.println("Time taken : " + (endTime - startTime) + " ms");  
+    public void testSolutionSimple() {
+        assertEquals("666,thenumberofthebeast", Simple.erase("666, the number of the beast"));
+        assertEquals("06   07651970", Simple.erase("06   07 65 19 70 "));
+        assertEquals("OK", Simple.erase("OK"));
+        assertEquals("Coucou  JM  B", Simple.erase("Cou cou  J M  B"));
+        assertEquals("", Simple.erase(""));
     }
     
-    
+    @Test
+    public void testSolutionEfficace() {
+        assertEquals("666,thenumberofthebeast", Efficace.erase("666, the number of the beast"));
+        assertEquals("06   07651970", Efficace.erase("06   07 65 19 70 "));
+        assertEquals("OK", Efficace.erase("OK"));
+        assertEquals("Coucou  JM  B", Efficace.erase("Cou cou  J M  B"));
+        assertEquals("", Efficace.erase(""));
+    }
 }
