fd96a8858de10743c42df351261f1b5c27fe80ec
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2022-06-14 15:56:35 +0200

    Approfondissement des tests Python

--- analyse/Python/analyse.py
+++ analyse/Python/analyse.py
@@ -1,16 +1,34 @@
 import time
+import algo.sobriete_102 as sob
+import algo.efficacite_13 as eff
 
-resultats = []
+def analyse(fonction):
+	resultats = []
 
-for test in range(100):
-	start = time.time()
+	with open('../texte 100k not erased.txt') as f:
+		input = f.read()
 
-	erase("Cou cou  J M  B")
+	with open('../texte 100k erased.txt') as f:
+		output = f.read()
 
-	end = time.time()
+	for test in range(100):
+		start = time.time()
 
-	resusltats.append(end - start)
+		erased = fonction(input)
 
-moy = sum(resultats)/len(resultats)
+		if erased != output:
+			print('Le résultat n\'est pas correct')
 
-print("Cela a prit {moy} ms")
+		end = time.time()
+
+		resultats.append(end - start)
+
+	moy = sum(resultats)/len(resultats)
+
+	print(f"Cela a prit {sum(resultats)} s, avec une moyenne de {moy*1000} ms")
+
+print('Analyse de sobriete_102')
+analyse(sob.erase)
+
+print('\nAnalyse de sobriete_102')
+analyse(eff.erase)
--- analyse/Python/test.py
+++ analyse/Python/test.py
@@ -1,20 +1,29 @@
-from algo.sobriete_102 import erase
 import codewars_test as test
+import algo.sobriete_102 as sob
+import algo.efficacite_13 as eff
 
-@test.describe('Sample tests')
-def sample_tests():
-    @test.it("erase: ''")
-    def test1():
-        test.assert_equals(erase(''), '')
-    
-    @test.it("erase: '06   07 65 19 70 '")
-    def test2():
-        test.assert_equals(erase('06   07 65 19 70 '), '06   07651970')
-    
-    @test.it("erase: '666, the number of the beast'")
-    def test3():
-        test.assert_equals(erase('666, the number of the beast'), '666,thenumberofthebeast')
+def tests(fonction):
+	@test.describe('Sample tests')
+	def sample_tests():
+		@test.it("erase: ''")
+		def test1():
+			test.assert_equals(fonction(''), '')
+		
+		@test.it("erase: '06   07 65 19 70 '")
+		def test2():
+			test.assert_equals(fonction('06   07 65 19 70 '), '06   07651970')
+		
+		@test.it("erase: '666, the number of the beast'")
+		def test3():
+			test.assert_equals(fonction('666, the number of the beast'), '666,thenumberofthebeast')
 
-    @test.it("erase: 'Cou cou  J M  B'")
-    def test4():
-        test.assert_equals(erase('Cou cou  J M  B'), 'Coucou  JM  B')
+		@test.it("erase: 'Cou cou  J M  B'")
+		def test4():
+			test.assert_equals(fonction('Cou cou  J M  B'), 'Coucou  JM  B')
+
+
+print('Test de sobriete_102')
+tests(sob.erase)
+
+print('\n\n\nTest de efficacite_13')
+tests(eff.erase)
